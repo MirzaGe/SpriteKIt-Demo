4bdbcc9c6031032e2a2678460eb0708afb86bfb5
Sherry1481 <shareyscorpi1@gmail.com>
2021-06-08 18:50:49 +0500

    red box

--- SpriteKIt Demo/GameScene.swift
+++ SpriteKIt Demo/GameScene.swift
@@ -11,10 +11,12 @@ import SpriteKit
 class GameScene: SKScene {
     
     let myFirstNode = SKNode()
+    let myFirstSpriteNode = SKSpriteNode(color: UIColor.red, size: CGSize(width: 200.0, height: 200.0))
    
     
     override func didMove(to view: SKView) {
         addChild(myFirstNode)
+        addChild(myFirstSpriteNode)
     }
     
    
